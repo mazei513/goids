0765b1f55efd2da9d6b09270880a3ac3e44d361c
Azraei Yusof <mazei513@gmail.com>
2025-02-16 17:00:02 +0800

    Not sure why it doesn't act boid-ey

--- main.go
+++ main.go
@@ -11,65 +11,102 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-const screenSize = 800
-const maxSpeed = 10
-const nBoids = 10
+const screenSize = 2000
+const maxSpeed = 20
+const nBoids = 1000
 
 type Boid struct{ x, y, dx, dy float64 }
 
 type Game struct {
-	boids []*Boid
+	boids            []*Boid
+	repulse, attract float64
+}
+
+func mag(dx, dy float64) float64 {
+	return math.Sqrt(dx*dx + dy*dy)
 }
 
 func (g *Game) Update() error {
-	for i, b := range g.boids {
-		// Rule 1
-		centerX, centerY := 0.0, 0.0
-		for j, b2 := range g.boids {
-			if i == j {
-				continue
-			}
-			centerX += b2.x
-			centerY += b2.y
-		}
-		centerX /= float64(len(g.boids)) - 1
-		centerY /= float64(len(g.boids)) - 1
-		v1x, v1y := (centerX-b.x)/100, (centerY-b.y)/100
+	// Input handling
+	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
+		g.attract -= 1
+	} else if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
+		g.attract += 1
+	}
 
-		// Rule 2
+	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
+		g.repulse -= 1
+	} else if ebiten.IsKeyPressed(ebiten.KeyDown) {
+		g.repulse += 1
+	}
+
+	// if !inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+	// 	return nil
+	// }
+
+	// Boid update
+	for i, b := range g.boids {
+		v1x, v1y := 0.0, 0.0
 		v2x, v2y := 0.0, 0.0
+		v3x, v3y := 0.0, 0.0
 		for j, b2 := range g.boids {
-			if i == j {
+			dx, dy := b2.x-b.x, b2.y-b.y
+			dMag := mag(dx, dy)
+			if i == j || dMag > 100 {
 				continue
 			}
-			dx, dy := b.x-b2.x, b.y-b2.y
-			if math.Sqrt(dx*dx+dy*dy) >= 16 {
-				continue
+			// Rule 1
+			v1x += b2.x
+			v1y += b2.y
+
+			// Rule 2
+			if dMag < g.repulse {
+				v2x -= dx
+				v2y -= dy
 			}
-			v2x -= dx
-			v2y -= dy
+
+			// Rule 3
+			v3x += b2.dx
+			v3y += b2.dy
 		}
+
+		// Rule 1
+		v1x /= nBoids - 1
+		v1y /= nBoids - 1
+		v1x, v1y = (v1x-b.x)/g.attract, (v1y-b.y)/g.attract
+
+		// Rule 3
+		v3x /= nBoids - 1
+		v3y /= nBoids - 1
+		v3x = (v3x - b.dx) / 16
+		v3y = (v3y - b.dy) / 16
 
 		// Update vector
-		b.dx += v1x + v2x
-		b.dy += v1y + v2y
-		fmt.Println(v1x, v1y, v2x, v2y, b.dx, b.dy)
+		b.dx += v1x + v2x + v3x
+		b.dy += v1y + v2y + v3y
+
+		// Limit max speed
+		dMag := mag(b.dx, b.dy)
+		if dMag > maxSpeed {
+			b.dx = (b.dx / dMag) * maxSpeed
+			b.dy = (b.dy / dMag) * maxSpeed
+		}
 
 		// Update movement
 		b.x += b.dx
 		b.y += b.dy
 
 		// Bounce off corner
-		if b.x < 0 || b.x > screenSize {
+		screenHalf := screenSize / 2.0
+		if b.x < -screenHalf || b.x > screenHalf {
 			b.dx = -b.dx
 			b.x += b.dx * 2
 		}
-		if b.y < 0 || b.y > screenSize {
+		if b.y < -screenHalf || b.y > screenHalf {
 			b.dy = -b.dy
 			b.y += b.dy * 2
 		}
 	}
-	fmt.Println()
 	return nil
 }
 
@@ -80,10 +117,10 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		opts.ColorScale.Scale(1, c, 1, 1)
 		opts.GeoM.Translate(-float64(boidImage.Bounds().Dx())/2, -float64(boidImage.Bounds().Dy())/2)
 		opts.GeoM.Rotate(math.Atan2(b.dy, b.dx) + math.Pi/2)
-		opts.GeoM.Translate(b.x, b.y)
+		opts.GeoM.Translate(b.x+screenSize/2, b.y+screenSize/2)
 		screen.DrawImage(boidImage, opts)
 	}
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %f, TPS: %f", ebiten.ActualFPS(), ebiten.ActualTPS()))
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %f, TPS: %f\nrepulse: %f, attract: %f", ebiten.ActualFPS(), ebiten.ActualTPS(), g.repulse, g.attract))
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -103,14 +140,18 @@ func main() {
 	}
 	boidImage = ebiten.NewImageFromImage(img)
 
-	ebiten.SetWindowSize(screenSize, screenSize)
+	ebiten.SetWindowSize(1000, 1000)
 	ebiten.SetWindowTitle("goids")
 	boids := make([]*Boid, nBoids)
 	for i := range boids {
 		dx := rand.Float64()
-		boids[i] = &Boid{rand.Float64() * screenSize, rand.Float64() * screenSize, dx, 1 - dx}
+		dy := rand.Float64()
+		dMag := mag(dx, dy)
+		dx = (dx / dMag) * maxSpeed
+		dy = (dy / dMag) * maxSpeed
+		boids[i] = &Boid{(rand.Float64() - 0.5) * screenSize, (rand.Float64() - 0.5) * screenSize, dx, dy}
 	}
-	err = ebiten.RunGame(&Game{boids: boids})
+	err = ebiten.RunGame(&Game{boids: boids, repulse: 20.0, attract: 1000.0})
 	if err != nil {
 		panic(err)
 	}
